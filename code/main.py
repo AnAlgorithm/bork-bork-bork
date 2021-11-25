11240234674ca47f6d532fa93eb02ff192a9525b
The Algorithmic <64064699+AnAlgorithm@users.noreply.github.com>
2021-11-25 20:58:39 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 # --- Init
 import re
-dictionary_names = ["sign","in","image","search","shopping","preferences","your","been","save","have","google","advanced","i'm","feeling","lucky","about"]
-dictionary_new = ["sine","ing","imege","seerch","shoeppeeng","prefferences","yuoor","beee","sefe","hefe-a","google-a","edfunced","ee'm","feeleeng","loocky","ebuoot"]
+dictionary_names = ["sign","in","image","search","shopping","preferences","your","been","save","have","google","advanced","i'm","feeling","lucky","about","more","info"]
+dictionary_new = ["sine","ing","imege","seerch","shoeppeeng","prefferences","yuoor","beee","sefe","hefe-a","google-a","edfunced","ee'm","feeleeng","loocky","ebuoot","mure-a","inffu"]
 
 # ---
 # --- Reading text
