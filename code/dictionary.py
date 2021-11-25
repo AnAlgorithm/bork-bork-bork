85aca55a29ff3e908501e088e41a22412f8a5287
The Algorithm <64064699+AnAlgorithm@users.noreply.github.com>
2021-11-25 13:05:39 +0530

    hey this doesn't do anything it's just a reference

---
+++ code/dictionary.py
@@ -0,0 +1,14 @@
+dictionary_names = ["sign","in","image","search","shopping","preferences","your","been","save","have","google"]
+dictionary_new = ["sine","ing","imege","seerch","shoeppeeing","prefferences","yuoor","beee","sefe","hefe-a","google-a"]
+
+# sign - sine
+# in - ing
+# image - imege
+# search - seerch
+# shopping - shoeppeeing
+# preferences - prefferences
+# your - yuoor
+# been - beee
+# save - sefe
+# have - hefe-a
+# google - google-a
