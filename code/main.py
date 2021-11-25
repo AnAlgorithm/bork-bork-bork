b8932976625e59c25bd0bc8579903b035cfe0108
The Algorithmic <64064699+AnAlgorithm@users.noreply.github.com>
2021-11-25 15:48:54 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 # --- Init
 import re
 dictionary_names = ["sign","in","image","search","shopping","preferences","your","been","save","have","google","advanced"]
-dictionary_new = ["sine","ing","imege","seerch","shoeppeeng","prefferences","yuoor","beee","sefe","hefe-a","google-a","edfenced"]
+dictionary_new = ["sine","ing","imege","seerch","shoeppeeng","prefferences","yuoor","beee","sefe","hefe-a","google-a","edfunced"]
 
 # ---
 # --- Reading text
