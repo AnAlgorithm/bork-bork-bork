c230bf0a92c49c1de6aeddfb02c07472be99a051
The Algorithmic <64064699+AnAlgorithm@users.noreply.github.com>
2021-11-25 14:27:21 +0530

    Update dictionary.py

--- code/dictionary.py
+++ code/dictionary.py
@@ -1,11 +1,11 @@
 dictionary_names = ["sign","in","image","search","shopping","preferences","your","been","save","have","google"]
-dictionary_new = ["sine","ing","imege","seerch","shoeppeeing","prefferences","yuoor","beee","sefe","hefe-a","google-a"]
+dictionary_new = ["sine","ing","imege","seerch","shoeppeeng","prefferences","yuoor","beee","sefe","hefe-a","google-a"]
 
 # sign - sine
 # in - ing
 # image - imege
 # search - seerch
-# shopping - shoeppeeing
+# shopping - shoeppeeng
 # preferences - prefferences
 # your - yuoor
 # been - beee
