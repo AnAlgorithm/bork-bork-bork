7446fe66668c651806e742b40d0586631cfbb1c4
AnAlgorithm <64064699+AnAlgorithm@users.noreply.github.com>
2021-11-25 10:41:55 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,3 +1,18 @@
+# --- Init
+import re
+dictionary = { "sign" : "sine" ,
+              "in" : "ing" ,
+              "image" : "imege" ,
+              "search" : "seerch" ,
+              "shopping" : "shoeppeeng" ,
+              "preferences" : "prefferences" ,
+              "your" : "yuoor" ,
+              "been" : "beee" ,
+              "save" : "sefed" ,
+              "have" : "hefe-a" ,
+              "google" : "google-a" }
+
+# ---
 # --- Reading text
 file = open("demofile.txt","rt")
 text = file.read()
@@ -5,3 +20,4 @@ file.close()
 
 # ---
 # --- Making changes to the text
+
