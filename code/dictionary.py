e51265a4a44fc54b15e39ff1a05bec17eac97158
AnAlgorithm <64064699+AnAlgorithm@users.noreply.github.com>
2021-11-26 12:14:56 +0530

    v2.0.0
    #4

--- code/dictionary.py
+++ code/dictionary.py
@@ -1,5 +1,5 @@
-dictionary_names = ["sign","in","image","search","shopping","preferences","your","been","save","have","google","advanced","i'm","feeling","lucky","about","more","info"]
-dictionary_new = ["sine","ing","imege","seerch","shoeppeeng","prefferences","yuoor","beee","sefe","hefe-a","google-a","edfunced","ee'm","feeleeng","loocky","ebuoot","mure-a","inffu"]
+dictionary_names = ["sign","in","image","search","shopping","preferences","your","been","save","have","google","advanced","i'm","feeling","lucky","about","more","info","shower","mostly","cloudy","sunny","little","snow","thunderstorm"]
+dictionary_new = ["sine","ing","imege","seerch","shoeppeeng","prefferences","yuoor","beee","sefe","hefe-a","google-a","edfunced","ee'm","feeleeng","loocky","ebuoot","mure-a","inffu","showeryguben","moostlee","cloodee","sonnee","leetl-a","snoo","doonderstoorm"]
 
 # sign - sine
 # in - ing
@@ -19,3 +19,10 @@ dictionary_new = ["sine","ing","imege","seerch","shoeppeeng","prefferences","yuo
 # about - ebuoot
 # more - mure-a
 # info - inffu
+# shower - showeryguben
+# mostly - mostly
+# cloudy - cloodee
+# sunny - sonnee
+# little - leetl-a
+# snow - snoo
+# thunderstorm - doonderstoorm
