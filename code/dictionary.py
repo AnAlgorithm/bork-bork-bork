4bdd82033336d3b2a9667d5a39294c4c43b81f95
AnAlgorithm <64064699+AnAlgorithm@users.noreply.github.com>
2022-01-29 13:28:05 +0530

    Update dictionary.py

--- code/dictionary.py
+++ code/dictionary.py
@@ -1,3 +1,6 @@
+
+# This is no program, rather a reference of the translations in the language.
+
 dictionary_names = ["sign","in","image","search","shopping","preferences","your","been","save","have","google","advanced","i'm","feeling","lucky","about","more","info","shower","mostly","cloudy","sunny","little","snow","thunderstorm","rain"]
 dictionary_new = ["sine","ing","imege","seerch","shoeppeeng","prefferences","yuoor","beee","sefe","hefe-a","google-a","edfunced","ee'm","feeleeng","loocky","ebuoot","mure-a","inffu","showeryguben","moostlee","cloodee","soonee","leetl-a","snoo","doonderstoorm","reen"]
 
