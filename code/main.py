de232222f14b4f68531a4c7be2c2d8e427803010
AnAlgorithm <64064699+AnAlgorithm@users.noreply.github.com>
2022-02-22 17:12:34 +0530

    text, code and comments is now more simpler

--- code/main.py
+++ code/main.py
@@ -3,23 +3,18 @@ import re
 dictionary_names = ["sign","in","image","search","shopping","preferences","your","been","save","have","google","advanced","i'm","feeling","lucky","about","more","info","shower","mostly","cloudy","sunny","little","snow","thunderstorm","rain"]
 dictionary_new = ["sine","ing","imege","seerch","shoeppeeng","prefferences","yuoor","beee","sefe","hefe-a","google-a","edfunced","ee'm","feeleeng","loocky","ebuoot","mure-a","inffu","showeryguben","moostlee","cloodee","soonee","leetl-a","snoo","doonderstoorm","reen"]
 
-# ---
 # --- Reading text
 file = open("demofile.txt","r") # Replace demofile with your file
 text = file.read()
 file.close()
 
-# ---
 # --- Making changes to the text
 i = 0
 for i in len(dictionary_names):
   text = re.sub(dictionary_names[i],dictionary_new[i],text)
   i = i + 1
   
-# ---
 # --- Overwriting file text with new one
 file0 = open("demofile.txt","w") # Replace demofile with your file
 file0.write(text)
 file0.close()
-
-# ---
